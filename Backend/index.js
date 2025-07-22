eafd1f520ac38a11071e83bceac1d28761cf5db2
Utkarsh-998 <guptautkarsh208@gmail.com>
2025-07-23 02:12:10 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,8 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  // origin: "http://localhost:5173",
+  origin:"https://chat-app-t7q5.onrender.com"
   credentials: true
 }));
 
